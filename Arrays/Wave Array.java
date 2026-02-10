7c3a7992420cbc7a50f5f2dd0fb279520019ab90
Sangeeta Varun <91114632+Sangeeta290499@users.noreply.github.com>
2026-02-10 16:32:12 +0530

    Implemented Wave Array

---
+++ Arrays/Wave Array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void sortInWave(int arr[]) {
+       
+      int n = arr.length;
+        if (n <= 1) return;
+
+        for (int i = 0; i < n; i += 2) {
+
+           
+            if (i > 0 && arr[i] < arr[i - 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i - 1];
+                arr[i - 1] = temp;
+            }
+
+            if (i < n - 1 && arr[i] < arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+            
+        }
+        
+    }
+}
