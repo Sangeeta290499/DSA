14d6c6a92da59ab423f8ad93679fcf594f35ca11
Sangeeta Varun <91114632+Sangeeta290499@users.noreply.github.com>
2026-02-14 18:37:58 +0530

    Add Roman to Integer conversion
    Implement Roman to Integer conversion logic using a HashMap to store symbol values and handle subtractive cases.

---
+++ Arrays/Roman to Integer.java
@@ -0,0 +1,34 @@
+// Roman to Integer
+
+// Store Roman symbols and their values in a HashMap.
+// Initialize a variable res = 0 to store the final result.
+// Traverse the string from left to right.
+// If current value is less than the next value, subtract it (subtractive case like IV, IX).
+// Otherwise, add the current value to the result.
+// TC: O(n) | SC: O(1)
+
+class Solution {
+    public int romanToInt(String s) {
+        
+        HashMap<Character, Integer>map = new HashMap<>();
+
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int res = 0;
+        for(int i = 0;i < s.length();i++){
+            if(i < s.length()-1 && map.get(s.charAt(i)) < map.get(s.charAt(i+1))){
+                res = res-map.get(s.charAt(i));
+            }
+            else{
+                 res = res+map.get(s.charAt(i));
+            }
+        }
+        return res;
+    }
+}
